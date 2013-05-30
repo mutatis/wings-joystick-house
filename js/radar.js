7e1ec0cdbd40ce2c8749a8afee2105f1d38e69f4
mutatisguga@gmail.com <mutatisguga@gmail.com@3aa12b8e-1957-0351-22c0-8be6aa1d15e2>
2013-05-30 14:45:37 +0000

    Só mudei um fps

--- js/radar.js
+++ js/radar.js
@@ -26,7 +26,7 @@ function Radar(file, tamanho_w, tamanho_h, posicao_x, posicao_y)
         this.current_frame = 0;
         this.frames = 29;
         
-        this.fps = 50;//frames por segundo
+        this.fps = 29;//frames por segundo
         this.time_per_frame = 1000/this.fps;
         this.setFPS = function(newFPS)
         {//abre setFPS
